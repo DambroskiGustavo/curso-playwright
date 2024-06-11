3f1e830c74e70eb40c27c92e471c84ac165ef5cb
Gustavo Dambroski <gustavodambroski@gmail.com>
2024-06-11 18:20:26 -0300

    new class about basic locators

--- tests/primeiro-test.spec.ts
+++ tests/primeiro-test.spec.ts
@@ -3,5 +3,13 @@ import { test } from "@playwright/test";
 // ***** Estrutura básica de teste no Playwright ***** //
 test('Visitando página do Playwright', async ({ page }) => {
     await page.goto('https://playwright.dev');
-    await page.locator('.getStarted_Sjon').click();
+
+    /* Forma de utilizar os locators via selectors convencionais */
+    // await page.locator('.getStarted_Sjon').click();
+    
+    /* Formas mais simples de encontrar elementos */
+    // await page.getByText('Get Started').click();
+    const text = await page.getByText('enables reliable end-to-end testing for modern web apps.')
+        .textContent();
+        console.log(text);
 })
