b9c6b44c954df706bfb81fabe25bdb7c0fda243c
Gustavo Dambroski <gustavodambroski@gmail.com>
2024-07-03 15:38:12 -0300

    new class to create and resolve some challenges

---
+++ tests/desafio-sauce-demo.spec.ts
@@ -0,0 +1,35 @@
+import { expect, test } from '@playwright/test'
+
+test('Desafio 1', async({ page }) => {
+    await page.goto('https://www.saucedemo.com');
+    await page.locator('[data-test="username"]').fill('standard_user');
+    await page.locator('[data-test="password"]').fill('secret_sauce');
+    await page.locator('input#login-button').click();
+
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+    const productsLabel = page.locator('[data-test="title"]');
+    await expect(productsLabel).toHaveText('Products');
+})
+
+test('Desafio 2', async({ page }) => {
+    const errorLabel = page.locator('[data-test="error"]');
+    await page.goto('https://www.saucedemo.com');
+    await expect(errorLabel).not.toBeVisible();
+    await page.locator('[data-test="username"]').fill('locked_out_user');
+    await page.locator('[data-test="password"]').fill('secret_sauce');
+    await page.locator('input#login-button').click();
+    
+    await expect(errorLabel).toHaveText('Epic sadface: Sorry, this user has been locked out.');
+    await expect(errorLabel).toBeVisible();
+})
+
+test('Desafio 3', async({ page }) => {
+    await page.goto('https://www.saucedemo.com');
+    await page.locator('[data-test="username"]').fill('standard_user');
+    await page.locator('[data-test="password"]').fill('123456');
+    await page.locator('input#login-button').click();
+
+    const errorLabel = page.locator('[data-test="error"]');
+    await expect(errorLabel).toHaveText('Epic sadface: Username and password do not match any user in this service');
+    await expect(errorLabel).toBeVisible();
+})
--- tests/saucedemo-expects.spec.ts
+++ tests/saucedemo-expects.spec.ts
@@ -7,12 +7,12 @@ test('Localizando por data-teste-id', async ({ page }) => {
 
 /* uso de Asserts, utilizando o "expect" */
 /* expect.soft = comando do qual não para o teste, mesmo com erro, ele continuará */
-test.only('Asserts básicos', async ({ page }) => {
+test('Asserts básicos', async ({ page }) => {
     await page.goto('https://www.saucedemo.com/');
     const loginbutton = await page.locator('input#login-button');
-    await expect.soft(loginbutton).toHaveCSS('background-color', 'rgb(225, 35, 26)');
-    await expect(loginbutton).not.toHaveCSS('background-color', 'rgb(0, 20, 0)');
+    //await expect.soft(loginbutton).toHaveCSS('background-color', 'rgb(225, 35, 26)');
+    // await expect(loginbutton).not.toHaveCSS('background-color', 'rgb(0, 20, 0)');
     await expect(loginbutton).toHaveAttribute('value','Login');
     await expect.soft(loginbutton, 'Botão está visível').not.toBeHidden();
-    await expect.soft(loginbutton).toHaveCSS('background-color', 'rgb(225, 35, 26)');
+    // await expect.soft(loginbutton).toHaveCSS('background-color', 'rgb(225, 35, 26)');
 })
