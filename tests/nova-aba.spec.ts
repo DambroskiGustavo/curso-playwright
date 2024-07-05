b2be2ddd884592276a22ba84ca8b3f2bae47e021
Gustavo Dambroski <gustavodambroski@gmail.com>
2024-07-04 22:39:56 -0300

    new class about to open and teste new tabs

---
+++ tests/nova-aba.spec.ts
@@ -0,0 +1,22 @@
+import { expect, test } from '@playwright/test'
+
+// eslint-disable-next-line playwright/expect-expect
+test('Controlando uma nova aba', async ({ page }) => {
+    // Navegando para URL
+    await page.goto('https://playwright.dev/');
+
+    // Definindo a promisse (evento) que vai aguardar pelo evento de criar uma nova página (tab)
+    const pagePromise = page.context().waitForEvent('page');
+    await page.locator('.gh-btn').getByText('Star').click();
+
+    // "Capturando" a nova página aberta para podermos controlá-la
+    const newPage = await pagePromise;
+    await newPage.waitForLoadState();
+
+    console.log(await newPage.title());
+    await expect(newPage).toHaveURL('https://github.com/microsoft/playwright');
+
+    await page.bringToFront();
+    await page.goto('https://www.google.com');
+    await expect(newPage).toHaveURL('https://www.google.com');
+})
